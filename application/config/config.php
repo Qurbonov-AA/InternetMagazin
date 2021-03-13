2b8699179b55f09f6f8a68c0aa4016157a3a63ca
Qurbonov-AA <akmal.q@jafton.com>
2021-03-13 20:05:19 +0500

    settings

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://newmagazin.uz/';
+$config['base_url'] = 'http://InternetMagazin/';
 
 /*
 |--------------------------------------------------------------------------
