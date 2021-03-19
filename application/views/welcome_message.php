1f9a8cccafcf661122ed9bb340d7e2e47146ee08
Abduraimov114 <oybek998997564934@gmail.com>
2021-03-19 09:03:02 +0500

    foter va menu

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -544,5 +544,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         mixins: [Vue2Filters.mixin],
     });
 </script>
+<footer class="footer-distributed">
+
+<div class="footer-left">
+
+    <h3>About<span>Gazzon.Uz</span></h3>
+
+    <p class="footer-links">
+        <a href="#">Home</a>
+        |
+        <a href="#">Blog</a>
+        |
+        <a href="#">About</a>
+        |
+        <a href="#">Contact</a>
+    </p>
+
+    <p class="footer-company-name">© 2021 Copyright: Gazzon.Uz </p>
+</div>
+
+<div class="footer-center">
+    <div>
+        <i class="fa fa-map-marker"></i>
+          <p><span>Manzil eski shaxar  va Gum savdo majmuasi</p>
+    </div>
+
+    <div>
+        <i class="fa fa-phone"></i>
+        <p>+9989***********</p>
+    </div>
+    <div>
+        <i class="fa fa-envelope"></i>
+        <p><a href="mailto:support@eduonix.com">Gazzonuz@gmail.com</a></p>
+    </div>
+</div>
+<div class="footer-right">
+    <p class="footer-company-about">
+        <span>About the company</span>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta veniam dignissimos corporis nihil accusamus numquam nisi et perferendis.</p>
+    <div class="footer-icons">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+    </div>
+</div>
+</footer>
 </body>
 </html>
