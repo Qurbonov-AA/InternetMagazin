2e8310a67d62df9761b1a603ccc31144439a7576
Tojiyev-BI <barattojiyev@gmail.com>
2021-03-19 18:23:22 +0500

    admin panelda til qushilyapti

--- application/views/admin/admin.php
+++ application/views/admin/admin.php
@@ -55,7 +55,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 </head>
 <body>
-		<div class="row">
+   <div class="admin-navbar">
+   <a href="http://InternetMagazin/"> <button @click="main_menu" class='btn btn-outline-success'><i class="fa fa-2x fa-home"> </i></button> </a> 
+       <button @click="change_language('ru')" class="fa fa-2x btn btn-outline-success">RU</button>
+       <button @click="change_language('uz')" class="fa fa-2x btn btn-outline-success">UZ</button>
+   </div>
+	     <div class="row">
 			<div class="col-md-3 col-lg-3 col-xl-3 col-3">
 				<ul class="list-group">
 				  <li class="list-group-item" name="users" id="user">Foydalanuvchi yaratish</li>
