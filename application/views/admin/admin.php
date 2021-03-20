c9b55090bc52222824193ce1648a7b6fa89bd054
Abduraimov114 <oybek998997564934@gmail.com>
2021-03-20 18:21:48 +0500

    admin qilindi

--- application/views/admin/admin.php
+++ application/views/admin/admin.php
@@ -55,7 +55,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 </head>
 <body>
-   <d iv class="admin-navbar">
+   <div class="admin-navbar">
    <a href="http://InternetMagazin/"> <button @click="main_menu" class='btn btn-outline-success'><i class="fa fa-2x fa-home"> </i></button> </a> 
        <button @click="change_language('ru')" class="fa fa-2x btn btn-outline-success">RU</button>
        <button @click="change_language('uz')" class="fa fa-2x btn btn-outline-success">UZ</button>
