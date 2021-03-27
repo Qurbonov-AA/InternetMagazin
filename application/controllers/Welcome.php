2ed0c0cd1f82e8c4b2a6873dfbcd9d1410128090
Qurbonov-AA <qurbonovakmalazamatovich@gmail.com>
2021-03-27 10:45:03 +0500

    grafikni drilldown qildim

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,11 +25,11 @@ class Welcome extends CI_Controller {
                     	WHERE g.id = b.id_good
                     	group by g.id")->result_array();
 					$data['xarajat_turi'] = $this->db->query("SELECT x.xarajat_turi, sum(h.xarajat_summ) as price FROM hisob as h, xarajat_turi as x
-
 														WHERE x.id = h.xarajat_nomi
-
 														GROUP BY x.id")->result_array();
-
+					$data['drilldown'] = $this->db->query("SELECT * FROM hisob h, xarajat_turi x
+					WHERE x.id = h.xarajat_nomi
+					ORDER BY h.xarajat_nomi")->result_array();
 					$this->load->view('admin/admin',$data);					
 				}
 	}
